17fe27dfc1d859f5d93fb708846f5f177e26eca8
Marc Lebrun <mclbn@users.noreply.github.com>
2023-07-03 16:43:38 +0200

    Update Nmap

--- nmap/build.sh
+++ nmap/build.sh
@@ -5,8 +5,8 @@ set -o pipefail
 set -x
 
 
-NMAP_VERSION=6.49BETA1
-OPENSSL_VERSION=1.0.2c
+NMAP_VERSION=7.94
+OPENSSL_VERSION=1.1.1u
 
 
 function build_openssl() {
@@ -18,7 +18,7 @@ function build_openssl() {
     cd openssl-${OPENSSL_VERSION}
 
     # Configure
-    CC='/opt/cross/x86_64-linux-musl/bin/x86_64-linux-musl-gcc -static' ./Configure no-shared linux-x86_64
+    CC='/musl-cross-make/output/bin/x86_64-linux-musl-gcc -static' ./Configure no-shared linux-x86_64
 
     # Build
     make
@@ -29,18 +29,18 @@ function build_nmap() {
     cd /build
 
     # Install Python
-    DEBIAN_FRONTEND=noninteractive apt-get update
-    DEBIAN_FRONTEND=noninteractive apt-get install -yy python
+    DEBIAN_FRONTEND=noninteractive apt-get update --allow-releaseinfo-change
+    DEBIAN_FRONTEND=noninteractive apt-get install -yy python3
 
     # Download
-    curl -LO http://nmap.org/dist/nmap-${NMAP_VERSION}.tar.bz2
+    curl -LO https://nmap.org/dist/nmap-${NMAP_VERSION}.tar.bz2
     tar xjvf nmap-${NMAP_VERSION}.tar.bz2
     cd nmap-${NMAP_VERSION}
 
     # Configure
-    CC='/opt/cross/x86_64-linux-musl/bin/x86_64-linux-musl-gcc -static -fPIC' \
-        CXX='/opt/cross/x86_64-linux-musl/bin/x86_64-linux-musl-g++ -static -static-libstdc++ -fPIC' \
-        LD=/opt/cross/x86_64-linux-musl/bin/x86_64-linux-musl-ld \
+    CC='/musl-cross-make/output/bin/x86_64-linux-musl-gcc -static -fPIC' \
+        CXX='/musl-cross-make/output/bin/x86_64-linux-musl-g++ -static -static-libstdc++ -fPIC' \
+        LD=/musl-cross-make/output/bin/x86_64-linux-musl-ld \
         LDFLAGS="-L/build/openssl-${OPENSSL_VERSION}"   \
         ./configure \
             --without-ndiff \
@@ -51,28 +51,30 @@ function build_nmap() {
 
     # Don't build the libpcap.so file
     sed -i -e 's/shared\: /shared\: #/' libpcap/Makefile
+    sed -i -e 's/shared\: /shared\: #/' libz/Makefile
 
     # Build
     make -j4
-    /opt/cross/x86_64-linux-musl/bin/x86_64-linux-musl-strip nmap ncat/ncat nping/nping
+    /musl-cross-make/output/bin/x86_64-linux-musl-strip nmap ncat/ncat nping/nping
 }
 
 function doit() {
+
+    # check if /output was mapped
+    if [ ! -d /output ]; then
+        echo "/output directory not found. Make sure you are running the container by mapping the output directory with '-v'"
+        exit -1
+    fi
+
     build_openssl
     build_nmap
 
-    # Copy to output
-    if [ -d /output ]
-    then
-        OUT_DIR=/output/`uname | tr 'A-Z' 'a-z'`/`uname -m`
-        mkdir -p $OUT_DIR
-        cp /build/nmap-${NMAP_VERSION}/nmap $OUT_DIR/
-        cp /build/nmap-${NMAP_VERSION}/ncat/ncat $OUT_DIR/
-        cp /build/nmap-${NMAP_VERSION}/nping/nping $OUT_DIR/
-        echo "** Finished **"
-    else
-        echo "** /output does not exist **"
-    fi
+    OUT_DIR=/output/`uname | tr 'A-Z' 'a-z'`/`uname -m`
+    mkdir -p $OUT_DIR
+    cp /build/nmap-${NMAP_VERSION}/nmap $OUT_DIR/
+    cp /build/nmap-${NMAP_VERSION}/ncat/ncat $OUT_DIR/
+    cp /build/nmap-${NMAP_VERSION}/nping/nping $OUT_DIR/
+    echo "** Finished **"
 }
 
 doit
